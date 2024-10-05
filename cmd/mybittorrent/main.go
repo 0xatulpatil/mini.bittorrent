ea33d459b168fe91befa2ef6ab7b5e85685d2b21
0xatulpatil <atuluttampatil.1757@gmail.com>
2024-10-05 18:12:42 +0530

    extend bencode to support list encodings

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -6,34 +6,91 @@ import (
 	"os"
 	"strconv"
 	"unicode"
-	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
 
 var _ = json.Marshal
 
-func decodeBencode(bencodedString string) (interface{}, error) {
-	if unicode.IsDigit(rune(bencodedString[0])) {
-		var firstColonIndex int
+func decodeString(bString string, start int) (string, int, error) {
+	var firstColonIndex int
 
-		for i := 0; i < len(bencodedString); i++ {
-			if bencodedString[i] == ':' {
-				firstColonIndex = i
-				break
-			}
+	for i := start; i < len(bString); i++ {
+		if bString[i] == ':' {
+			firstColonIndex = i
+			break
+		}
+	}
+
+	lengthStr := bString[start:firstColonIndex]
+
+	lengthOfString, err := strconv.Atoi(lengthStr)
+	if err != nil {
+		return "", start, err
+	}
+
+	return bString[firstColonIndex+1 : firstColonIndex+1+lengthOfString], firstColonIndex + lengthOfString + 1, nil
+}
+
+func decodeInt(bString string, start int) (int, int, error) {
+	if start == len(bString) {
+		return start, 0, fmt.Errorf("Bad bencoded Int")
+	}
+
+	lastIdxOfInt := start
+
+	for lastIdxOfInt < len(bString) && bString[lastIdxOfInt] != 'e' {
+		lastIdxOfInt++
+	}
+	if lastIdxOfInt >= len(bString) || bString[lastIdxOfInt] != 'e' {
+		return start, 0, fmt.Errorf("Bad bencoded Int")
+	}
+
+	lastIdxOfInt++
+	dInt, err := strconv.Atoi(bString[start+1 : lastIdxOfInt-1])
+
+	return dInt, lastIdxOfInt, err
+}
+
+func decodeList(bString string, start int) (interface{}, int, error) {
+	// l4:hello3atuli3ee -> ["hello", "atul", 3]
+	decodedList := make([]interface{}, 0)
+	if start >= len(bString) {
+		return decodedList, start, fmt.Errorf("Invalid bencoded list")
+	}
+	currIdx := start
+	currIdx++
+
+	for {
+		if currIdx >= len(bString) {
+			return nil, currIdx, fmt.Errorf("bad bencoded list")
 		}
 
-		lengthStr := bencodedString[:firstColonIndex]
+		if bString[currIdx] == 'e' {
+			break
+		}
 
-		length, err := strconv.Atoi(lengthStr)
+		decodedVal, nextStartIdx, err := decodeBencode(bString, currIdx)
 		if err != nil {
-			return "", err
+			return nil, start, err
 		}
+		currIdx = nextStartIdx
 
-		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
-	} else if bencodedString[0] == 'i' && bencodedString[len(bencodedString)-1] == 'e' {
-		return strconv.Atoi(bencodedString[1 : len(bencodedString)-1])
-	} else {
-		return "", fmt.Errorf("Only strings are supported at the moment")
+		decodedList = append(decodedList, decodedVal)
+
+	}
+
+	return decodedList, currIdx, nil
+}
+
+func decodeBencode(bencodedString string, start int) (interface{}, int, error) {
+	switch {
+	case unicode.IsDigit(rune(bencodedString[start])):
+		return decodeString(bencodedString, start)
+	case bencodedString[start] == 'i':
+		return decodeInt(bencodedString, start)
+	case bencodedString[0] == 'l':
+		return decodeList(bencodedString, start)
+	default:
+		return "", len(bencodedString), fmt.Errorf("Only strings are supported at the moment")
 	}
 }
 
@@ -44,7 +101,7 @@ func main() {
 
 		bencodedValue := os.Args[2]
 
-		decoded, err := decodeBencode(bencodedValue)
+		decoded, _ , err := decodeBencode(bencodedValue, 0)
 		if err != nil {
 			fmt.Println(err)
 			return
